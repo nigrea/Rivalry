42f166d42c8abef3827c11b673bd8ee152a2b72e
Alfyyy <alf421va@yahoo.com>
2015-05-01 19:36:02 -0400

    Profile ranked wins test

--- profile.php
+++ profile.php
@@ -6,6 +6,7 @@ include 'header.php';
 <div class="jumbotron">
 	<h1><?php print $_SESSION['main_summoner_name']?></h1>
 	
+
 	<?php 
 	print $_SESSION ['user_id'];
 	$summoner = get_summoner_by_name ( $_SESSION['main_summoner_name'] );
@@ -15,16 +16,25 @@ include 'header.php';
 	$challengertier = get_challengertier();
 	?>
 	
-	<img src=<?php print $config_image_summoner_icon.$summoner->$lower_case_name->profileIconId.".png" ?>>
+	<p><img src=<?php print $config_image_summoner_icon.$summoner->$lower_case_name->profileIconId.".png" ?>></p>
 	
+	<h2>
 	
+	<?php 
+	$wins = ( $statssummary->playerStatSummaries[8]->wins);
+	print "Ranked Wins: $wins";
+		?>
+	</h2>
 	
 	<p><?php
+	
 	var_dump ( $summoner );
 	var_dump ( $rankstats );
-	var_dump ( $statssummary);
-	
+	var_dump ( $statssummary->playerStatSummaries[8]);
+
 	?></p>
+	
+
 
 </div>
 </div>
